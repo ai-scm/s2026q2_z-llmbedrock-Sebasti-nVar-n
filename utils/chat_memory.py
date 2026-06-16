ed306bc02299516baec9ece802b982da7ec82959
Juan Sebastiàn Varòn Arenas <Sebastian.Varon@blend360.com>
2026-06-16 16:48:32 -0500

    Create chat_memory.py

---
+++ utils/chat_memory.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def init_memory():
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+def add_message(role, content):
+    st.session_state.messages.append({"role": role, "content": content})
